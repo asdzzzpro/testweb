547f19c209d9dfb3371dce3fe7f0a71f24719087
Huy Nguyễn Quang <huydnf@gmail.com>
2022-07-11 18:10:59 +0700

    tomcat

---
+++ SaleWebV1/src/main/java/com/mycompany/configs/DispatcherServletInitializer.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.configs;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ *
+ * @author Qhuy
+ */
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return null;
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[]{
+//            WebAppConTextConfig.class
+//        };
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[] {"/"};
+//    }
+//    @Override
+//    protected 
+}
+ 
